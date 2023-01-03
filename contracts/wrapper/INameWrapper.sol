98e1df5842ea263184dfd907e13ac48e2d0a9d07
serenae.eth <serenae.eth@protonmail.com>
2023-01-03 12:16:18 -0500

    Now allowing the parent owner to call setExpiry too.
    When called by the parent owner, no fuses are checked, as the parent can always extend expiry for a subname.

--- contracts/wrapper/INameWrapper.sol
+++ contracts/wrapper/INameWrapper.sol
@@ -16,6 +16,7 @@ uint32 constant CANNOT_CREATE_SUBDOMAIN = 32;
 uint32 constant PARENT_CANNOT_CONTROL = 1 << 16;
 uint32 constant IS_DOT_ETH = 1 << 17;
 uint32 constant CAN_EXTEND_EXPIRY = 1 << 18;
+uint32 constant CAN_EXTEND_EXPIRY_WITH_CONTROLLER = 1 << 19;
 uint32 constant CAN_DO_EVERYTHING = 0;
 uint32 constant PARENT_CONTROLLED_FUSES = 0xFFFF0000;
 // all fuses apart from IS_DOT_ETH
